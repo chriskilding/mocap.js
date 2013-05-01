fb5814cacc6b1f626f7632a3c2a2faf0353929ef
Christopher Kilding <mr.chris.k@gmail.com>
2013-05-01 23:11:13 +0100

    tidy up data recorder tests

--- tests/datasource/DataRecorderTests.js
+++ tests/datasource/DataRecorderTests.js
@@ -4,8 +4,6 @@ define(function (require) {
 	// Import dependencies.
 	var DataRecorder = require("mocap/datasource/DataRecorder");
 	
-    var user = JSON.parse(require("text!mocap/sample-user.json"));
-    
     var recorder;
   
 	// Define the QUnit module and lifecycle.
@@ -14,10 +12,8 @@ define(function (require) {
             // Testing timeouts is messy
             // so don't bother with the custom time value for now
             recorder = new DataRecorder();
-            //sinon.spy(bcaster.vent.rawData, "dispatch");
         },
         teardown: function () {
-            //bcaster.vent.rawData.dispatch.restore(); // For safety
             recorder = null;
         }
 	});
