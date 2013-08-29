3cad4b794c45fb4e3ff101eb1c7b70c5bd5f41e7
Christopher Kilding <mr.chris.k@gmail.com>
2013-08-29 20:16:01 +0100

    remove the console logging, add warning about arbitrary nature of data

--- mocap/datasource/ServerSentEventsSource.js
+++ mocap/datasource/ServerSentEventsSource.js
@@ -1,6 +1,8 @@
 // Another networked skeleton data source;
 // this allows one-way streaming capture from a Server-Sent Events data source
 // (SSE is also known as the EventSource API).
+// Remember, SSE just sends and receives strings...
+// so if you wanted JSON, you as the class user will need to JSON.parse() it.
 define([
     'underscore'
 ], function (_) {
@@ -13,9 +15,7 @@ define([
     }
 
     ServerSentEventsSource.prototype.start = function () {
-        this.source.addEventListener('message', _.bind(function (e) {
-            console.log(e);
-            
+        this.source.addEventListener('message', _.bind(function (e) {            
             // No point broadcasting null or invalid data
             if (e && e.data) {
                 this.bcaster.broadcastUser(e.data);
